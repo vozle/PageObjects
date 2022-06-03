88fc63b75f8165405df1eec9c5d6982a629731fd
vozle <vozle2@gmail.com>
2022-06-03 15:47:40 +0300

    Add data

---
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -0,0 +1,41 @@
+package ru.netology.web.data;
+
+import lombok.Value;
+
+public class DataHelper {
+
+    private DataHelper() {
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class CardInfo {
+        public String cardNumber;
+    }
+
+    public static CardInfo getFirstCardNumber() {
+        return new CardInfo("5559 0000 0000 0001");
+    }
+
+    public static CardInfo getSecondCardNumber() {
+        return new CardInfo("5559 0000 0000 0002");
+    }
+}
