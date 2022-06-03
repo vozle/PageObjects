cc8a216f93a773c71ac8723eab777c957ebce349
vozle <vozle2@gmail.com>
2022-06-03 15:48:09 +0300

    Add pages

---
+++ src/test/java/ru/netology/web/page/TransferPage.java
@@ -0,0 +1,27 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TransferPage {
+    private SelenideElement transferButton = $("[data-test-id='action-transfer']");
+    private SelenideElement amountInput = $("[data-test-id='amount'] input");
+    private SelenideElement fromInput = $("[data-test-id='from'] input");
+    private SelenideElement transferHead = $(byText("Пополнение карты"));
+
+    public TransferPage() {
+        transferHead.shouldBe(visible);
+    }
+
+    public DashboardPage doTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        amountInput.setValue(amountToTransfer);
+        fromInput.setValue(cardInfo.getCardNumber());
+        transferButton.click();
+        return new DashboardPage();
+    }
+
+}
